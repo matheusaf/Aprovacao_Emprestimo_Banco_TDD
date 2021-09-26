3df7dbd6333760de437bcf56a63a3d7c09981c2e
Vinícius Amendoeira Ferraria <39192461+viniferraria@users.noreply.github.com>
2021-09-26 02:43:15 +0000

    ajuste travis

---
+++ aprovacao_banco/src/main/java/com/aprovacao_banco/Cliente/Cliente.java
@@ -0,0 +1,11 @@
+package com.cliente;
+
+public class Cliente {
+	public Cliente(String nome, int idade, float saldo, bool bomPagador, bool concessao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.saldo = saldo;
+        this.bomPagador = bomPagador;
+        this.concessao = concessao;
+	}
+}
