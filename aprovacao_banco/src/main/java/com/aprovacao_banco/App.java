1bdf93fd0c442089db2adbf07426db173a6c1a81
Matheus Amendoeira Ferraria <matheusaf8@gmail.com>
2021-09-25 22:13:30 -0300

    starting project

---
+++ aprovacao_banco/src/main/java/com/aprovacao_banco/App.java
@@ -0,0 +1,13 @@
+package com.aprovacao_banco;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
