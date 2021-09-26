0b84727d40709020e64d3fa934e61147a383891f
Vinícius Amendoeira Ferraria <39192461+viniferraria@users.noreply.github.com>
2021-09-26 04:11:37 +0000

    criação dos testes otimizados

--- src/test/java/com/aprovacao_banco/EmprestimoClienteTest.java
+++ src/test/java/com/aprovacao_banco/EmprestimoClienteTest.java
@@ -18,16 +18,30 @@ public class EmprestimoClienteTest
      }
     
      /**
-     * Rigorous Test :-)
+     * Um sistema bancário é utilizado para avaliar o cliente e determinar se um empréstimo pode ser liberado para ele. As regras para liberação do empréstimo são as seguintes:
+     * O saldo médio deve ser maior que R$1000,00 mensais.
+     * Mesmo se o saldo médio ser menor que o exigido, mas o cliente possuí um histórico de bom pagador o empréstimo também é liberado.
+     * Se um cliente possui uma concessão especial de crédito ele também pode ter empréstimo liberado, mesmo não tendo saldo mínimo exigido e não sendo um bom pagador.
+     * Para qualquer outro caso o cliente tem o empréstimo negado.
      **/
     @Test
-    public void testeLiberaEmprestimo()
+    public void testeLiberaEmprestimoSaldo()
     {
-        assertEquals("Libera Empréstimo", emprestimoCliente.aprovaEmprestimo(new Cliente("Eidolas", 22, 1000.00, true, true )));
+        assertEquals("Libera Empréstimo", emprestimoCliente.aprovaEmprestimo(new Cliente("Eidolas", 22, 1000.00, false, false )));
     }
 
     @Test
-    public void testeNaoLibera(){
-        asserEquals("Não Libera", emprestimoCliente.aprovaEmprestimo(new Cliente("Eidolas", 22, 1000.00, true, true )));
+    public void testeLiberaEmprestimoBomPagador() {
+        asserEquals("Libera Empréstimo", emprestimoCliente.aprovaEmprestimo(new Cliente("Eidolas", 22, 100.00, true, false )));
+    }
+
+    @Test
+    public void testeLiberaEmprestimoConcessao(){
+        asserEquals("Libera Empréstimo", emprestimoCliente.aprovaEmprestimo(new Cliente("Eidolas", 22, 100.00, false, true )));
+    }
+
+    @Test
+    public void testeNaoLiberaEmprestimo(){
+        asserEquals("Não Libera", emprestimoCliente.aprovaEmprestimo(new Cliente("Eidolas", 22, 100.00, false, true )));
     }
 }
